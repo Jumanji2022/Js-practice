703c85e016283e9dcad6abe0515212ce5b7491cd
Js study <programmer.from.france@gmail.com>
2022-06-13 13:20:44 +0300

    study 3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 "use stript";
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
@@ -11,12 +10,36 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотенных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотенных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
 
-      console.log(personalMovieDB);
+      for (let i = 0; i < 2; i++) {
+             const  a = prompt("Один из последних просмотенных фильмов?", ""),
+                    b = prompt("На сколько оцените его?", "");
+
+                    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50)  {
+                        personalMovieDB.movies[a] = b;
+                        console.log('done');
+                    } else {
+                        console.log('error');
+                        i--;
+                    }
+
+            
+      }
+
+      if (personalMovieDB.count < 10 && personalMovieDB.count >= 1) {
+        console.log('мало фильмов'); 
+      } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('вы средний зритель');
+      } else if (personalMovieDB.count >= 30) {
+        console.log('да вы киноман мазафака!');
+      } else if (personalMovieDB.count < 1) {
+        console.log('Ану бысторо смотреть!');
+      } else {
+        console.log('ошибка!');
+      }
+      
+       
+     
+
+      console.log(personalMovieDB); 
